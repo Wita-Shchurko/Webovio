fbd9dfbd72fd375be992a0c29fd13ae5631c5eb2
Vitaliia Shchurko <vitaliya.shchurko@gmail.com>
2023-03-30 20:08:56 +0300

    popup added

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,9 @@ const ul = document.querySelector('#parent'),
       numberOfChildren = lis.length,
       overlayNav = document.querySelector('.overlay-navigation'),
       openOverlay = document.querySelector('.open-overlay'),
+      openPopup = document.querySelectorAll('.popup-opener'),
+      popup = document.querySelector('.popup'),
+      popupClose = document.querySelector('.popup__close'),
       anchors = document.querySelectorAll('a[href*="#"]');
 
 openOverlay.addEventListener('click', () => {
@@ -23,7 +26,7 @@ for (let anchor of anchors) {
     anchor.addEventListener('click', function (e) {
       e.preventDefault()
       
-      const blockID = anchor.getAttribute('href').substr(1)
+      const blockID = anchor.getAttribute('href').substring(1)
       
       document.getElementById(blockID).scrollIntoView({
         behavior: 'smooth',
@@ -32,4 +35,17 @@ for (let anchor of anchors) {
         overlayNav.classList.toggle('activeMenu');
         overlayNav.classList.toggle('inactiveMenu');
     })
-  }
+}
+
+for (let link of openPopup) {
+    link.addEventListener('click', (e) => {
+        e.preventDefault()
+    
+        popup.style.display = `block`;
+    })
+}
+    
+
+popupClose.addEventListener('click', () => {
+    popup.style.display = `none`;
+})
